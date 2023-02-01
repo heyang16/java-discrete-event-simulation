1ae84c813ce49e60ef553e4e4b316014c050ad65
heyang16 <54947648+heyang16@users.noreply.github.com>
2023-02-01 11:31:32 +0000

    Finished extension

--- src/ssq/Departure.java
+++ src/ssq/Departure.java
@@ -7,6 +7,7 @@ public class Departure implements Event<SingleServerQueue> {
   @Override
   public void invoke(SingleServerQueue simulation) {
     simulation.decrementQueue();
+    simulation.incrementTotalService();
     System.out.println("Departure at " + simulation.getCurrentTime() + " new population = "
         + simulation.getQueueLength());
     if (simulation.getQueueLength() > 0) {
--- src/ssq/SingleServerQueue.java
+++ src/ssq/SingleServerQueue.java
@@ -7,29 +7,43 @@ public class SingleServerQueue extends Simulation<SingleServerQueue> {
 
   private final Random generator;
   private final double duration;
+  private final int serviceLimit;
   private int queueLength;
   private double cumulativeQueueLength;
   private double queueLengthRecordedUntil;
+  private int totalServices;
   public final static double SERVICE_TIME = 0.25;
 
-  public SingleServerQueue(long seed, double duration) {
+  public SingleServerQueue(long seed, double duration, int serviceLimit) {
     super();
     this.generator = new Random(seed);
     this.duration = duration;
+    this.serviceLimit = serviceLimit;
     this.queueLength = 0;
     this.cumulativeQueueLength = 0;
     this.queueLengthRecordedUntil = 0;
+    this.totalServices = 0;
     this.schedule(new Arrival(), getNextArrival());
   }
 
   @Override
   protected boolean stop() {
-    return diary.peek().getTime() > duration;
+    return diary.peek().getTime() > duration || totalServices > serviceLimit;
   }
 
   @Override
   public SingleServerQueue getState() {
     return this;
+  }
+
+  @Override
+  public void simulate() {
+    super.simulate();
+    if (totalServices <= serviceLimit) {
+      currentTime = duration;
+      updateCumulativeQueueLength();
+    }
+    System.out.println("SIMULATION COMPLETE - the mean queue length was " + getMeanQueueLength());
   }
 
   public double getNextArrival() {
@@ -41,9 +55,8 @@ public class SingleServerQueue extends Simulation<SingleServerQueue> {
    * by 1.
    */
   public void incrementQueue() {
-    cumulativeQueueLength += (currentTime - queueLengthRecordedUntil) * queueLength;
+    updateCumulativeQueueLength();
     queueLength += 1;
-    queueLengthRecordedUntil = currentTime;
   }
 
   /**
@@ -51,9 +64,17 @@ public class SingleServerQueue extends Simulation<SingleServerQueue> {
    * length by 1.
    */
   public void decrementQueue() {
-    cumulativeQueueLength += (currentTime - queueLengthRecordedUntil) * queueLength;
+    updateCumulativeQueueLength();
     queueLength -= 1;
+  }
+
+  public void updateCumulativeQueueLength() {
+    cumulativeQueueLength += (currentTime - queueLengthRecordedUntil) * queueLength;
     queueLengthRecordedUntil = currentTime;
+  }
+
+  public void incrementTotalService() {
+    totalServices += 1;
   }
 
   public int getQueueLength() {
@@ -61,8 +82,8 @@ public class SingleServerQueue extends Simulation<SingleServerQueue> {
   }
 
   public double getMeanQueueLength() {
-    cumulativeQueueLength += (duration - getCurrentTime()) * queueLength;
-    return cumulativeQueueLength / duration;
+    cumulativeQueueLength += (duration - currentTime) * queueLength;
+    return cumulativeQueueLength / currentTime;
   }
 
   /**
@@ -72,8 +93,14 @@ public class SingleServerQueue extends Simulation<SingleServerQueue> {
   public static void main(String[] args) {
     long seed = Long.parseLong(args[0]);
     double duration = Double.parseDouble(args[1]);
-    SingleServerQueue s = new SingleServerQueue(seed, duration);
+    SingleServerQueue s = new SingleServerQueue(seed, duration, Integer.MAX_VALUE);
     s.simulate();
-    System.out.println("SIMULATION COMPLETE - the mean queue length was " + s.getMeanQueueLength());
+  }
+
+  public static void mainExtension(String[] args) {
+    long seed = Long.parseLong(args[0]);
+    int serviceLimit = Integer.parseInt(args[1]);
+    SingleServerQueue s = new SingleServerQueue(seed, Double.MAX_VALUE, serviceLimit);
+    s.simulate();
   }
 }
